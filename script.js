5fc2890a7d686ac76e9156decfc9a1a668b6a99e
uzmamstf <49521264+uzmamstf@users.noreply.github.com>
2019-09-01 22:07:17 +0500

    WIP

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5651b311f4bde3b478a18fbf9b55efcd`)
-    .then(function(response) {
-        // handle success  
-        document.querySelector('p').innerHTML = response.data.weather[0].main;
-        document.querySelector('p').innerHTML = `City: ${response.data.name}`;
-        document.querySelector('p').innerHTML = `Now: ${response.data.weather[0].description}`
+ let getInputValue = querySelector('input').value;
 
-    })
-    .catch(function(error) {
-        alert("City not found");
-    })
-    .finally(function() {
-        // always executed
-    });
+ axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5651b311f4bde3b478a18fbf9b55efcd`)
+     .then(function(response) {
+         // handle success  
+         document.querySelector('p').innerHTML = response.data.weather[0].main;
+         document.querySelector('p').innerHTML = `City: ${response.data.name}`;
+         document.querySelector('p').innerHTML = `Now: ${response.data.weather[0].description}`
+
+     })
+     .catch(function(error) {
+         alert("City not found");
+     })
+     .finally(function() {
+         // always executed
+     });
